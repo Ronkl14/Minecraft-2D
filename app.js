3cae610e90f12b843ca3050ec421db0cc1cbd547
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-02-03 12:14:08 +0200

    added abillity to mine tiles

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import { world1 } from "./worlds.js";
 
 const tileGrid = document.querySelector(".blocks");
+const axe = document.querySelector(".axe");
+const pickaxe = document.querySelector(".pickaxe");
+const shovel = document.querySelector(".shovel");
+
+let currTool;
 
 const dirt = {
   tool: "shovel",
@@ -58,18 +63,42 @@ function numToTile(num) {
   }
 }
 
+axe.addEventListener("click", function () {
+  currTool = "axe";
+  console.log(currTool);
+});
+
+shovel.addEventListener("click", function () {
+  currTool = "shovel";
+  console.log(currTool);
+});
+
+pickaxe.addEventListener("click", function () {
+  currTool = "pickaxe";
+  console.log(currTool);
+});
+
+function checkMatch(tile, currTool) {
+  return tile.classList.contains(currTool) ? true : false;
+}
+
+function removeTile(tile) {
+  tile.classList.add("removed");
+}
+
 for (let i = 0; i < world1.length; i++) {
   for (let j = 0; j < world1[i].length; j++) {
     const currentTile = numToTile(world1[i][j]);
     const tileDiv = document.createElement("div");
     tileGrid.appendChild(tileDiv);
-    // tileDiv.style.gridRowStart = i + 1;
-    // tileDiv.style.gridRowEnd = i + 2;
-    // tileDiv.style.gridColumnStart = j + 1;
-    // tileDiv.style.gridColumnEnd = j + 2;
+    tileDiv.style.gridRowStart = i + 1;
+    tileDiv.style.gridRowEnd = i + 2;
+    tileDiv.style.gridColumnStart = j + 1;
+    tileDiv.style.gridColumnEnd = j + 2;
     tileDiv.classList.add(currentTile.class);
     tileDiv.classList.add(currentTile.tool);
     tileDiv.addEventListener("click", function (event) {
+      checkMatch(event.target, currTool) ? removeTile(event.target) : "";
     });
   }
 }
